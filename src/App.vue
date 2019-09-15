ac56286b580181c8a73bc7068ee62c369a747df0
denizerisen <38030540+denizerisen@users.noreply.github.com>
2019-09-15 15:37:28 +0300

    created repo
    had sass issues

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SlideText/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SlideText from './components/SlideText.vue';
+
+@Component({
+  components: {
+    SlideText,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="sass">
+  @import 'sass/main.sass'
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    background: $lilac
+
+</style>
---
+++ src/components/SlideText.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="slide__left">
+    <h1 class="slide__header">{{this.headerText}}</h1>
+    <h2 class="slide__subheader">{{this.headerText}}</h2>
+    <p class="slide__text">{{this.headerText}}</p>
+  
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class SlideText extends Vue { 
+        headerText: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+        subheaderText: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+        text: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla dignissi vulputate volutpat massa, ut tincidunt nunc convallis a. Suspendisse dapibus diam sit amet nisl gravida. '
+    }
+
+</script>
+
+<style lang="sass">
+    .slide__header
+        background: no-repeat url(../assets/header-text-background.svg)
+</style>
