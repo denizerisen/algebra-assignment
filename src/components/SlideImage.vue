f1e988416662b539e11219f8002df9d9e8e40a2e
denizerisen <38030540+denizerisen@users.noreply.github.com>
2019-09-15 19:07:32 +0300

    created base layout
    will clean up and fix errors tomorrow

---
+++ src/components/SlideImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="slide__right">
+        <img class="first" src="../assets/yellow-circle.svg" />
+        <img class="second" src="../assets/image-background.svg" width="100%" />
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class SlideImage extends Vue { 
+       
+    }
+
+</script>
+
+<style lang="sass">
+    .first
+        position: absolute
+        right: 0
+        top: 0
+    .second
+        position: relative
+        z-index: 2
+</style>
--- src/components/SlideText.vue
+++ src/components/SlideText.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="slide__left">
-    <h1 class="slide__header">{{this.headerText}}</h1>
-    <h2 class="slide__subheader">{{this.headerText}}</h2>
-    <p class="slide__text">{{this.headerText}}</p>
+    <div class="header">
+    <img  src="../assets/header-text-background.svg" width="100%"/>
+    <h1 class="slide__header">{{headerText}}</h1>
+    </div>
+    <h2 class="slide__subheader">{{this.subheaderText}}</h2>
+    <p class="slide__text">{{this.text}}</p>
   
+        <input type="text" class="input" v-model="headerText"/>
+        <input type="text" class="input" v-model="subheaderText"/>
+        <input type="text" class="input" v-model="text"/>
   </div>
 </template>
 
@@ -20,6 +26,26 @@
 </script>
 
 <style lang="sass">
+    .slide__left
+        font-family: 'Chewy', cursive
+        color: white
+        padding: 1em
+        text-align: center
+    .header
+        max-height: 170px
+    .slide__header, .slide__subheader
+        font-weight: 100
     .slide__header
-        background: no-repeat url(../assets/header-text-background.svg)
+        font-size: 4em
+        line-height: 1
+        transform: translateY(100%) translateY(-5.2em)
+        
+    .slide__subheader
+        font-size: 35px
+        margin: 0
+        padding: 0 10px
+    .slide__text
+        font-family: 'Gaegu', cursive
+        text-align: left
+        font-size: 30px
 </style>
